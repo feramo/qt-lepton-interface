ac08213bc87fcedea6cbaa750201bf2680bb6ec7
feramo <feramo@gmail.com>
2015-08-24 23:09:22 -0300

    Added two more pallets: linear and log thermal scales

--- Palettes.h
+++ Palettes.h
@@ -5,5 +5,7 @@ extern const int colormap_rainbow[];
 extern const int colormap_grayscale[];
 extern const int colormap_ironblack[];
 extern const int colormap_truergb[];
+extern const int colormap_maplin[];
+extern const int colormap_maplog[];
 
 #endif
